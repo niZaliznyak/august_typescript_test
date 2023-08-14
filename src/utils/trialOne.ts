e842bce6709a8e8430db3bfa92f8669d2b1746b1
Mykyta Zalizniak <57563554+niZaliznyak@users.noreply.github.com>
2023-08-14 12:38:07 +0300

    Update trialOne.ts

--- src/utils/trialOne.ts
+++ src/utils/trialOne.ts
@@ -1,7 +1,7 @@
 type objectKeys = keyof typeof Object;
 type valuesType = (typeof Object)[objectKeys];
 
-export const trialOne = <T>(obj: Object, path: string, initial?: T | undefined): valuesType | T => {
+export const trialOne = <T>(obj: Object, path: string, initial?: T): valuesType | T => {
   const result = path.split('.').reduce((prev, cur) => prev && prev[cur], obj);
   if (result === undefined && initial !== undefined) {
     return initial;
